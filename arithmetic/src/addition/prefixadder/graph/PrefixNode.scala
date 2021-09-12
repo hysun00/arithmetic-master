6c9e100e240661c0deb6d5315855e01dab3be71d
Haoyuan Sun <haoyuansun00@gmail.com>
2021-09-12 22:40:40 +0100

    Some annotations have been added.

--- arithmetic/src/addition/prefixadder/graph/PrefixNode.scala
+++ arithmetic/src/addition/prefixadder/graph/PrefixNode.scala
@@ -2,18 +2,18 @@ package addition.prefixadder.graph
 
 import scala.math.Ordered.orderingToOrdered
 
-/**
-  * The nodes of a prefix graph is known as PrefixNode.
-  * There are several nodes in the same logic level of a prefix graph.
-  * A node has several fan-ins and fan-outs.
-  * Some nodes in the implement certain logic operations, mapping inputs from fan-ins into some different outputs,
-    while all the others act as a buffer.
-  * The logic depth of a node is represented by level.
-  * Fan-ins, also the father nodes of a prefix node, are called fathers.
-  * Output of the node is called prefixData.
-  * The nodes of the same level are sortable with the help of index
-  * */
-
+/** This represents the node of a prefix graph.
+  * Every node corresponds to certain associative logic operations except those in the zero layer.
+  * Buffer nodes are not present.
+  * There are several nodes in the same layer/level of a prefix graph. A node has several fan-ins and fan-outs.
+  *
+  * @param level represents the logic depth of a prefix paragraph.
+  * @param fathers contains all the father nodes, i.e. fan-ins, of a child node. They are the nodes directly engaged in the
+  *                operation to get this node.
+  * @param prefixData records all the zero-layer nodes involved in the operation to get this node.
+  * @param index helps sort different nodes in the [[PrefixGraph]], which is constructed as a sequence.
+  *
+  */
 case class PrefixNode(level: Int, fathers: Set[PrefixNode], prefixData: Set[Int], index: Int)
      extends Ordered[PrefixNode] {
   val bit:Int = prefixData.max
@@ -22,33 +22,42 @@ case class PrefixNode(level: Int, fathers: Set[PrefixNode], prefixData: Set[Int]
 }
 
 
-/**
-  * Major construction method of PrefixNode is defined in this companion object.
-  * */
+/** Major construction method of [[PrefixNode]] is defined in its companion object.
+  */
 object PrefixNode {
   var index = 0
 
-  /**
-    * Receives a variable numbers of PrefixNodes, and gernerate a child PrefixNode
-    *
-    * @param father a variable number of PrefixNodes
-    *
-    * @return a PrefixNode
-    **/
+  /** Receives a variable numbers of PrefixNodes, and gernerates their child nodes based on certain rules.
+    * @param father a variable number of PrefixNode
+    * @return PrefixNode
+    */
   def apply(father: PrefixNode*): PrefixNode = {
     require(father.map(_.prefixData).reduce(_ intersect _).isEmpty, "father's prefixData has same prefixes")
     require(
       father.flatMap(_.prefixData).toList.sorted.zipWithIndex.map { case (idx, ele) => ele - idx }.toSet.size == 1,
       "prefixData is not continues"
     )
+
+    /** Its 'level' must be one plus the largest level of its father nodes, since buffer nodes are not considered.
+      * All its father nodes is sorted to compose the 'fathers'
+      * All the 'prefixData' of its father nodes are lumped and sorted to form its own 'prefixData'.
+      * */
     new PrefixNode(father.map(_.level).max + 1, father.sorted.toSet, father.flatMap(_.prefixData).toSet, indexInc)
   }
 
+  /**Performs increment of the index from zero.
+    * */
   def indexInc = {
     index = index + 1
     index - 1
   }
 
+  /** Produces the zero-layer nodes. Every node in the zero layer represents a unique bit (element)
+    * of the input sequence.
+    *
+    * @param bit indicates the particular bit that the PrefixNode corresponds to.
+    * @return a PrefixNode in the zero-layer of a prefix graph
+    */
   def apply(bit: Int): PrefixNode = {
     new PrefixNode(0, Set(), Set(bit), indexInc)
   }
---
+++ arithmetic/src/addition/prefixadder/graph/PrefixSumWithGraph.scala
@@ -0,0 +1,40 @@
+package addition.prefixadder.graph
+
+import addition.prefixadder.PrefixSum
+import chisel3._
+
+trait HasPrefixSumWithGraphImp extends PrefixSum {
+  val prefixGraph: PrefixGraph
+
+  /** Works out the sequence of prefix sum based on the given prefix graph.
+    *
+    * @param summands the input sequence, also the values which the zero-layer nodes hold.
+    * @return the sequence of the final prefix sum.
+    */
+  override def apply(summands: Seq[(Bool, Bool)]): Vector[(Bool, Bool)] = {
+    require(summands.size == prefixGraph.width, "Module width is different to Graph width")
+
+    /** Makes the Hash table that maps all the nodes to their output boolean pairs, i.e. GP pairs.
+      *
+      * @param level level of the nodes
+      * @param x Hash table mapping prefix nodes to their output, i.e., operation results
+      *
+      * @return the final Hash table containing the complete sequence of prefix sum
+      */
+    def helper(level: Int, x: Map[PrefixNode, (Bool, Bool)]): Map[PrefixNode, (Bool, Bool)] = if (
+      level > prefixGraph.depth
+    ) x
+    else
+      helper(
+        level + 1,
+        x ++ prefixGraph.level(level).map(node => node -> associativeOp(node.fathers.map(node => x(node)).toSeq)).toMap
+      )
+
+    helper(1, prefixGraph.level(0).zip(summands).toMap)
+      .filter(dict => prefixGraph.lastLevelNode.contains(dict._1))
+      .toSeq
+      .sortBy(_._1.bit)
+      .map(_._2)
+      .toVector
+  }
+}
